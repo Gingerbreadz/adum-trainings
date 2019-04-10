45f7e75ae33a04dfe18d2dd5e3de38e0b71ed4b5
Anatole Lefort <anatole.lefort@gmail.com>
2019-04-10 18:39:10 +0200

    parser: fetch & process each course's page to extract text content

--- parser.sh
+++ parser.sh
@@ -18,6 +18,11 @@ TMPDIR=`mktemp -d -p /tmp -t tmp.adumparser.XXXXXXXXXX` || {
 
 require_file $ADUM_COOKIE_FILE
 
+REGEX_RM_LEADING_SPACES='s/^\s\+//g'
+REGEX_RM_TRAILING_SPACES='s/\s\+$//g'
+REGEX_RM_EMPTY_LINES='/^$/d'
+REGEX_FORMAT_KEYS='s/\(.*\S\)\s\?:$/\1:/g'
+
 fetch_page() {
     url=$1
     outfile=$2
@@ -26,6 +31,26 @@ fetch_page() {
 
     curl -s -b $cookie $url > $outfile
     tidy -ashtml -latin1 -q --show-warnings no -m $outfile
+}
+
+process_module() {
+    mod=$1
+    shift 1
+
+    mod_id=`echo $mod | cut -d "=" -f2`
+    mod_tmp_html="$TMPDIR/mod_${mod_id}.html"
+    mod_tmp_file="$TMPDIR/mod_${mod_id}.txt"
+
+    fetch_page $ADUM_URL$mod $mod_tmp_html
+
+    xmllint --html $mod_tmp_html \
+            --xpath "/html/body/div/form/div/div/div/table/tr/td//text()" \
+        >> $mod_tmp_file
+
+    sed -i $mod_tmp_file \
+            -e $REGEX_RM_LEADING_SPACES \
+            -e $REGEX_RM_EMPTY_LINES \
+            -e $REGEX_FORMAT_KEYS
 }
 
 
@@ -37,6 +62,6 @@ modules=`xmllint --html $catalogue \
                  --xpath "/html/body/div/form/div/div/div/table/tr/td/a/@href" \
        | cut -d '"' -f2`
 
-for mod in $modules ; do echo $ADUM_URL$mod >> $OUTFILE ; done
+for mod in $modules ; do process_module $mod ; done
 
 rm -rf $TMPDIR
